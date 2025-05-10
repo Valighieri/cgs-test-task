3462270965aa2a356a058d1967f7f8a8fd21defb
Valentyn Uvarov <valentyn.y.uvarov@gmail.com>
2025-05-10 18:11:19 +0300

    initial commit

---
+++ src/main/java/com/example/cgstest/CgsTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgsTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgsTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgstest/CgsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
